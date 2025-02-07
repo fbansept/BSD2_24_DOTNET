7142582f582deeaa5650c681cdc79b8a2b770328
fbansept <bansept.franck@gmail.com>
2025-02-07 08:53:54 +0100

    Ajoutez des fichiers projet.

---
+++ BSD2_24/Data/BSD2_24Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using BSD2_24.Models;
+
+namespace BSD2_24.Data
+{
+    public class BSD2_24Context : DbContext
+    {
+        public BSD2_24Context (DbContextOptions<BSD2_24Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<BSD2_24.Models.Produit> Produit { get; set; } = default!;
+    }
+}
---
+++ BSD2_24/Models/Produit.cs
@@ -0,0 +1,8 @@
+﻿namespace BSD2_24.Models
+{
+    public class Produit
+    {
+        public int Id { get; set; }
+        public string Nom { get; set; }
+    }
+}
